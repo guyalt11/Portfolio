9d50c298ebe3660e522417a4992a57f4020d279c
guyalt11 <a@b.com>
2025-04-30 12:19:13 +0200

    revert changes to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Portfolio/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src') // This tells Vite where the "@" symbol points to
-    }
-  }
-})
-
-//import { defineConfig } from "vite";
-//import react from "@vitejs/plugin-react-swc";
-//import path from "path";
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
+//import path from 'path'
 //
 //// https://vitejs.dev/config/
-//export default defineConfig(({ mode }) => ({
-//  server: {
-//    host: "::",
-//    port: 8080,
-//  },
-//  plugins: [
-//    react(),
-//  ].filter(Boolean),
+//export default defineConfig({
+//  base: '/Portfolio/',
+//  plugins: [react()],
 //  resolve: {
 //    alias: {
-//      "@": path.resolve(__dirname, "./src"),
-//    },
-//  },
-//}));
+//      '@': path.resolve(__dirname, 'src') // This tells Vite where the "@" symbol points to
+//    }
+//  }
+//})
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
